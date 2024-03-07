32081aead5b20fb00b793a4f886cf0a50e675df6
fiwo1 <finn.wohnig@gmail.com>
2024-03-07 20:17:30 +0100

    First changes, creation of lists

--- Informatik Projekt.py
+++ Informatik Projekt.py
@@ -7,11 +7,27 @@
 # authors: Paula Stegmayer, Finn Wohnig, Paul Schabram
 # date:  07.03.2024
 # brief:   This program is a vocab-trainer. It is designed and programmed to train different languages and different topic-specific vocabulary
+
 print("Menu:")
-print("English (1)")
-print("French  (2)")
-print("Spanish (3)")
-print("Create your own vocab-list (4)")
+print("English (1)" + "\n" + "French (2)" + "\n" + "Spanish (3)" + "\n" + "Create your own vocab-list")
 
-int(input("Select a Language or create your own vocabs to continue (1 - 4): "))
+mode = int(input("Select a Language or create your own vocabs to continue (1 - 4): "))
 
+language = ["English", "French", "Spanish", "No language chosen"] 
+english = ["In the Kitchen", "Out in the nature", "Mechanical English", "Random Vocabs"]
+french = ["Dans la cuisine", "Dans la nature", "Francais mécanique", "Vocabulaire aléatoire"]
+spanish = ["En la cocina", "En la naturaleza", "Espanol mecánico", "Vocabulario aleatorio"]
+
+menu2 = print("Language chosen: " , language[mode-1] + "\n" + "Vocab-Package 1 ()" + "\n" + "Vocab-Package 2" \
+              + "\n" + "Vocab-Package 1" + "\n" + "Vocab-Package 1" + "\n" + "Vocab-Package 1")
+
+if mode == 1:
+    print(menu2)
+elif mode == 2:
+    print(menu2)
+elif mode == 3:
+    print(menu2)
+elif mode == 4:
+    print(menu2)
+elif mode > 4:
+    print("Sorry, this programm currently has only 4 modes available!")
