bf46e2d0adba2cc1a8d746ab1c4149969bee9ba8
fiwo1 <finn.wohnig@gmail.com>
2024-03-07 18:38:22 +0100

    first commit

--- Informatik Projekt.py
+++ Informatik Projekt.py
@@ -7,7 +7,16 @@
 # authors: Paula Stegmayer, Finn Wohnig, Paul Schabram
 # date:  07.03.2024
 # brief:   This program is a vocab-trainer. It is designed and programmed to train different languages and different topic-specific vocabulary
+print("Menu:")
+print("English (1)")
+print("French  (2)")
+print("Spanish (3)")
+print("Create your own vocab-list (4)")
+
+int(input("Select a Language or create your own vocabs to continue (1 - 4): "))
+
 print("Test")
+
 print("Testformerging")
 print("Funktioniert")
 #teständerung
